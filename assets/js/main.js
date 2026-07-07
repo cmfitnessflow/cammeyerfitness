dc01b425a31fe2d81bb77e803b639ac979dc420b
cmfitnessflow <cammeyerfit@gmail.com>
2026-07-07 11:25:48 -0400

    Add files via upload
    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,9 @@
+
 const toggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.primary-nav');
-
 if (toggle && nav) {
   toggle.addEventListener('click', () => {
     const open = nav.classList.toggle('open');
     toggle.setAttribute('aria-expanded', String(open));
-  });
-
-  nav.querySelectorAll('a').forEach((link) => {
-    link.addEventListener('click', () => {
-      nav.classList.remove('open');
-      toggle.setAttribute('aria-expanded', 'false');
-    });
   });
 }
